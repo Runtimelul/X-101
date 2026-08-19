452e81c51751589fd114126e9ec5be13957f382c
fahri1616 <162914610+fahri1616@users.noreply.github.com>
2026-08-19 17:58:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function getRndInteger(min, max) {
 let counter = 0;
 
 const makeCommits = (n) => {
-  if (n === 0) return;
+  if (n === 0) return4
   counter++;
   console.log(`Commit #${counter}`);
   
